ec46e0f40fc789e801f9a5553ee2a7562715df50
hichour2 <aminehichour@gmail.com>
2017-09-04 03:31:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 		<script src="https://apis.google.com/js/platform.js"></script>
+		<link rel="icon" type="image/x-icon" href="favicon.ico" />
+
 		
 	</head>
 	<body>
